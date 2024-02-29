8a051319793d690fde6f9c29d6f5586373ac008d
liu <liuhe36@gmail.com>
2024-02-29 21:26:33 +0800

    DEV

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,44 @@
       <router-view />
     </div>
   </div>
+  <el-button plain @click="dialogVisible = true">
+    Click to open the Dialog
+  </el-button>
+
+  <el-dialog
+      v-model="dialogVisible"
+      title="Tips"
+      width="500"
+      :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+
+const dialogVisible = ref(false)
+
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+      .then(() => {
+        done()
+      })
+      .catch(() => {
+        // catch error
+      })
+}
+</script>
 
 <style lang="less">
 #appindex {
