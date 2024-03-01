789fd3d38159e9903e3bb6b62cfcdfff6702e3f4
liu <liuhe36@gmail.com>
2024-03-01 14:56:03 +0800

    DEV

--- src/components/business/user/LoginForm.vue
+++ src/components/business/user/LoginForm.vue
@@ -14,9 +14,31 @@
             <div class="slider-wrap">
               <el-tabs v-model="active" @tab-click="handleClick">
                 <el-tab-pane label="账号密码登录" name="0">
-                  <LoginForm />
+                  <el-form :model="form" :rules="rules" ref="form" label-width="80px">
+                    <el-form-item label="用户名" prop="username">
+                      <el-input v-model="form.username"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                      <el-input type="password" v-model="form.password"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                      <el-button type="primary" @click="login">登录</el-button>
+                    </el-form-item>
+                  </el-form>
                 </el-tab-pane>
                 <el-tab-pane label="手机验证码登录" name="1">
+                  <el-form :model="form" :rules="rules" ref="form" label-width="80px">
+                    <el-form-item label="手机号" prop="phone">
+                      <el-input v-model="form.phone"></el-input>
+                    </el-form-item>
+                    <el-form-item label="验证码" prop="code">
+                      <el-input v-model="form.code"></el-input>
+                      <el-button type="primary" @click="getCode" :disabled="codeDisabled">{{ codeText }}</el-button>
+                    </el-form-item>
+                    <el-form-item>
+                      <el-button type="primary" @click="login">登录</el-button>
+                    </el-form-item>
+                  </el-form>
                 </el-tab-pane>
               </el-tabs>
             </div>
@@ -28,7 +50,7 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { ElMessageBox, ElDialog, ElTabPane, ElTabs } from 'element-plus'
+import { ElMessageBox} from 'element-plus'
 
 const dialogVisible = ref(false)
 
