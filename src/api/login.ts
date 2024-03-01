e42a277c16f7b19f06f9d862360d9c8dfe8a5077
liu <liuhe36@gmail.com>
2024-03-01 16:57:07 +0800

    DEV

--- src/api/login.ts
+++ src/api/login.ts
@@ -6,4 +6,18 @@ export const login = (params: Type.Object = {}) => fetch('login', params, 'post'
 //注册
 export const register = (params: Type.Object = {}) => fetch('register', params, 'post')
 
+// //获取验证码
+// export const getCode = (params: Type.Object = {}) => fetch('get_code', params, 'post')
+//
+// //忘记密码
+// export const forgetPassword = (params: Type.Object = {}) => fetch('forget_password', params, 'post')
+//
+// //修改密码
+// export const changePassword = (params: Type.Object = {}) => fetch('change_password', params, 'post')
+//
+// //退出登录
+// export const logout = (params: Type.Object = {}) => fetch('logout', params, 'post')
+
+//获取用户信息
+export const getUserInfo = (params: Type.Object = {}) => fetch('getUserInfo', params, 'post')
 
