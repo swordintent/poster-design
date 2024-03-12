361b2a7b1c7dfe602423b82996de0c902251cdee
liu <liuhe36@gmail.com>
2024-03-12 16:58:56 +0800

    DEV

--- src/api/github.ts
+++ src/api/github.ts
@@ -6,6 +6,8 @@
  * @LastEditTime: 2023-08-10 10:33:59
  */
 import fetch from '@/utils/axios'
+import Qiniu from "@/common/methods/QiNiu";
+
 const cutToken = 'ghp_qpV8PUxwY7as4jc'
 
 const reader = new FileReader()
@@ -19,18 +21,32 @@ function getBase64(file: File) {
   })
 }
 
-const putPic = async (file: any) => {
+async function uploadGit(file: any) {
   const repo = 'shawnphang/files'
   const d = new Date()
   const content = typeof file === 'string' ? file : await getBase64(file)
   const path = `${d.getFullYear()}/${d.getMonth()}/${d.getTime()}${file.name?.split('.').pop() || '.png'}`
   const imageUrl = 'https://api.github.com/repos/' + repo + '/contents/' + path
-  const body = { branch: 'main', message: 'upload', content, path }
+  const body = {branch: 'main', message: 'upload', content, path}
   const res = await fetch(imageUrl, body, 'put', {
     Authorization: `token ${cutToken}AqYfNFb6G2f2OVl4IVFOY`,
     'Content-Type': 'application/json; charset=utf-8',
   })
   return res?.content?.download_url || `https://fastly.jsdelivr.net/gh/shawnphang/files@main/${path}`
+}
+
+const putPic = async (file: any) => {
+  return upload({ file })
+}
+
+const options= { bucket: 'platform-test-1', prePath: 'user' }
+const upload = ({ file }: any) => {
+  if (file) {
+    const result: any = Qiniu.upload(file, options, (res: Type.Object) => {
+      console.log('res', res);
+    })
+    return result;
+  }
 }
 
 export default { putPic }
