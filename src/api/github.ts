3088142ca8155ad9b0cf7fa710ca9e3186f8b585
liu <liuhe36@gmail.com>
2024-03-12 18:17:39 +0800

    DEV

--- src/api/github.ts
+++ src/api/github.ts
@@ -43,11 +43,35 @@ const options= { bucket: 'platform-test-1', prePath: 'user' }
 const upload = ({ file }: any) => {
   if (file) {
     console.log('req', file.type, file);
-    const result: any = Qiniu.upload(file, options, (res: Type.Object) => {
+    const result: any = Qiniu.upload(base64ToFile(file, uuid()), options, (res: Type.Object) => {
       console.log('res', res);
     })
     return result;
   }
+}
+
+const uuid = () => {
+  const s:any = [];
+  const hexDigits = "0123456789abcdef";
+  for (let i = 0; i < 36; i++) {
+    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+  }
+  s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[8] = s[13] = s[18] = s[23] = "-";
+
+  return s.join("");
+}
+
+const base64ToFile = (base64: string, fileName: string) => {
+  const bstr = atob(base64);
+  let n = bstr.length;
+  const  u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], fileName);
 }
 
 export default { putPic }
